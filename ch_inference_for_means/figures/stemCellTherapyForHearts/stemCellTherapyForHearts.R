2fc258655643d56d2ef7839b1747e48460c2f6e4
OpenIntroOrg <openintro.org@gmail.com>
2015-01-26 20:16:24 -0800

    Update ch_inference_for_means figures and code

--- ch_inference_for_means/figures/stemCellTherapyForHearts/stemCellTherapyForHearts.R
+++ ch_inference_for_means/figures/stemCellTherapyForHearts/stemCellTherapyForHearts.R
@@ -1,26 +1,47 @@
 library(openintro)
 data(COL)
+data(stem.cells)
+d <- stem.cells
 
-d <- read.delim('stemCell.txt')
+change <- d$after - d$before
+t.test(change ~ d[, 1])
 
-change <- d[,3] - d[,2]
-#plot(change ~ d[,1])
-t.test(change ~ d[,1])
+myPDF('stemCellTherapyForHearts.pdf', 8, 2.7,
+      mar = c(3.1, 1.8, 3, 2),
+      mgp = c(2, 0.45, 0),
+      mfrow = c(1, 2))
+histPlot(change[d[, 1] == 'esc'],
+         breaks = seq(-10, 15, 2.5),
+         xlim = c(-10, 15),
+         axes = FALSE,
+         xlab = '',
+         main = '',
+         col = COL[1])
+axis(1, cex.axis = 0.85)
+mtext('Embryonic stem cell transplant',
+      line = 1.5,
+      cex = 1.25)
+mtext('Percent change in heart pumping function', 1,
+      line = 1.5,
+      cex = 0.9)
+par(mgp = c(2, 0.6, 0))
+axis(2, at = 0:3, cex.axis = 0.925)
 
-myPDF('stemCellTherapyForHearts.pdf', 8, 2.7, mar=c(3.1, 1.8, 3, 2), mgp=c(2, 0.45, 0), mfrow=c(1,2), las=1)
-histPlot(change[d[,1] == 'esc'], xlim=c(-10, 15), axes=FALSE, main='', breaks=seq(-10, 15, 2.5), xlab='', col=COL[1])
-axis(1, cex.axis=0.85)
-mtext('Embryonic stem cell transplant', line=1.5, cex=1.25)
-mtext('Percent change in heart pumping function', 1, line=1.5, cex=0.9)
-par(mgp=c(2, 0.6, 0))
-axis(2, at=0:3, cex.axis=0.925)
-
-par(mar=c(3.1, 1.8, 3, 0.5))
-histPlot(change[d[,1] == 'ctrl'], xlim=c(-10, 15), axes=FALSE, main='', breaks=seq(-10, 15, 2.5), xlab='', col=COL[1])
-axis(1, cex.axis=0.85)
-par(mgp=c(2, 0.6, 0))
-axis(2, at=0:3, cex.axis=0.925)
-mtext('Control (no treatment)', line=1.5, cex=1.25)
-mtext('Percent change in heart pumping function', 1, line=1.5, cex=0.9)
-
+par(mar = c(3.1, 1.8, 3, 0.5))
+histPlot(change[d[, 1] == 'ctrl'],
+         breaks = seq(-10, 15, 2.5),
+         xlim = c(-10, 15),
+         axes = FALSE,
+         xlab = '',
+         main = '',
+         col = COL[1])
+axis(1, cex.axis = 0.85)
+par(mgp = c(2, 0.6, 0))
+axis(2, at = 0:3, cex.axis = 0.925)
+mtext('Control (no treatment)',
+      line = 1.5,
+      cex = 1.25)
+mtext('Percent change in heart pumping function', 1,
+      line = 1.5,
+      cex = 0.9)
 dev.off()
