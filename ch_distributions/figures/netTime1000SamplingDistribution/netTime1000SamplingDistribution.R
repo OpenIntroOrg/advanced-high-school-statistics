d0fe7513173b704c96f17830c6646ae28fd36e2b
OpenIntroOrg <openintro.org@gmail.com>
2015-03-03 23:07:45 -0800

    Update with review of revisions to Distributions chapter

--- ch_distributions/figures/netTime1000SamplingDistribution/netTime1000SamplingDistribution.R
+++ ch_distributions/figures/netTime1000SamplingDistribution/netTime1000SamplingDistribution.R
@@ -14,27 +14,21 @@ for (i in 1:1000) {
 
 plot(0, 0,
      type = 'n',
-     xlim = c(88, 102),
+     xlim = c(70, 125),
      ylim = c(0, 145),
      xlab = 'Sample mean',
      ylab = 'Frequency',
      axes = FALSE)
 m <- mean(run10$time, na.rm = TRUE)
 s <- sd(run10$time, na.rm = TRUE)/10
-rect(m - s / 100, 0, m + s / 100, 1500,
-     col = '#00000044',
-     border = '#00000000')
-rect(m - s, 0, m + s, 1500,
-     col = '#00000011',
-     border = '#00000000')
-rect(m - 2 * s, 0, m + 2 * s, 1500,
-     col = '#00000011',
-     border = '#00000000')
-rect(m - 3 * s, 0, m + 3 * s, 1500,
-     col = '#00000011',
-     border = '#00000000')
 histPlot(means, col = COL[1], breaks = 25, add = TRUE)
 abline(h = 0)
-axis(1, at = c(90, 95, 100))
-axis(2, at = c(0, 50, 100))
+axis(1, at = seq(0, 200, 10))
+axis(1, at = seq(0, 200, 10) + 5, rep("", 21), tcl = -0.15)
+axis(2, at = c(0, 50, 100, 150))
+text(112, 75,
+     paste("The distribution of sample means,",
+           "shown here, is much narrower than",
+           "the distribution of raw observations.",
+           sep = "\n"))
 dev.off()
