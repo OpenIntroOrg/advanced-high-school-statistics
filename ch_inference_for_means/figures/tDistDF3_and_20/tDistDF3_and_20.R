2cff52a0c3002d24d4011380f301619591c6a935
OpenIntroOrg <openintro.org@gmail.com>
2015-04-04 16:28:30 -0700

    Review edits, update a figure for examples, and add Casio calculator instructions

--- ch_inference_for_means/figures/tDistDF3_and_20/tDistDF3_and_20.R
+++ ch_inference_for_means/figures/tDistDF3_and_20/tDistDF3_and_20.R
@@ -1,20 +1,19 @@
 library(openintro)
 data(COL)
 
-myPDF('tDistDF20RightTail1Point65.pdf', 6.8, 1.9,
+myPDF('tDistDF3_and_20.pdf', 6.8, 1.9,
       mar = c(1.6, 1, 0.05, 1),
       mgp = c(5, 0.45, 0),
       mfrow = c(1, 2))
-normTail(U = 1.65,
-         df = 12,
+normTail(U = 10,
+         df = 4,
          xlim = c(-4, 4),
          col = COL[1],
          axes = FALSE)
 axis(1)
-normTail(L = -3,
-         U = 3,
-         df = 2.3,
-         xlim = c(-4.5, 4.5),
+normTail(U = 1.65,
+         df = 12,
+         xlim = c(-4, 4),
          col = COL[1],
          axes = FALSE)
 axis(1)
