0801372e094fc9c24282a084ac0442fdb0948d5d
OpenIntroOrg <openintro.org@gmail.com>
2015-05-09 23:00:16 -0700

    Add sidedness plot and update .gitignore to ignore .Rhistory files

---
+++ ch_inference_foundations/figures/sidedness/sidedness_example_figures.R
@@ -0,0 +1,21 @@
+library(openintro)
+data(COL)
+
+myPDF("sidedness_example_figures.pdf", 6, 1,
+      mar = c(1.7, 1, 0.5, 1),
+      mgp = c(0.5, 0.6, 0),
+      mfrow = c(1, 3))
+normTail(L = -1.2,
+         col = COL[1],
+         xlab = expression(H[A]* ": " * p < "null value"),
+         axes = FALSE)
+normTail(U = 1.2,
+         col = COL[1],
+         xlab = expression(H[A]* ": " * p > "null value"),
+         axes = FALSE)
+normTail(L = -1.2,
+         U = 1.2,
+         col = COL[1],
+         xlab = expression(H[A]* ": " * p != "null value"),
+         axes = FALSE)
+dev.off()
