10e3a73fce590cbd5ad7968f70fd7a148bed2691
OpenIntroOrg <openintro.org@gmail.com>
2015-05-11 21:26:40 -0700

    Tweak formatting for print edition

--- ch_regr_simple_linear/figures/marioKartNewUsed/marioKartNewUsed.R
+++ ch_regr_simple_linear/figures/marioKartNewUsed/marioKartNewUsed.R
@@ -5,7 +5,7 @@ mk      <- marioKart[marioKart$totalPr < 100, ]
 mk$cond <- relevel(mk$cond, "used")
 cond <- as.numeric(ifelse(mk$cond == "new", 1, 0))
 
-myPDF('marioKartNewUsed.pdf', 4.5, 4,
+myPDF('marioKartNewUsed.pdf', 4.2, 2.8,
       mar = c(3, 3.5, 0, 0.5),
       mgp = c(1.9, 1.5 ,0))
 dotPlot(mk$totalPr, cond,
@@ -33,8 +33,8 @@ rect(-10, -1000, -0.125, 1000,
 rect(10, -1000, 1.125, 1000,
      border  =  rgb(1, 1, 1),
      col  =  rgb(1, 1, 1))
-text(0.48, 41.8,
+text(0.5, 41,
      expression(widehat(price) *" = 42.87 + 10.90     cond_new"),
-     cex = 0.8)
-points(0.605, 41.5, pch = 4, cex = 0.9)
+     cex = 0.75)
+points(0.625, 40.5, pch = 4, cex = 0.8)
 dev.off()
