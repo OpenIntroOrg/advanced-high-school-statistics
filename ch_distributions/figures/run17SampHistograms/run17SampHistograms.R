867719c6f57fdaf8dfa028622e64136026625358
Leah <leah.dorazio@sfuhs.org>
2019-05-13 09:40:56 -0700

    updating from run10 to run17

--- ch_distributions/figures/run17SampHistograms/run17SampHistograms.R
+++ ch_distributions/figures/run17SampHistograms/run17SampHistograms.R
@@ -1,19 +1,17 @@
 library(openintro)
 data(COL)
-
 data(run17)
-#d <- run17samp
- set.seed(50)
- run17samp <- run17[sample(1:nrow(run17), 100), ]
- 
-myPDF("run17sampHistograms.pdf", 7, 2.5,
-      mar = c(3.3, 3.5, 0.5, 1.5),
+
+set.seed(1)
+run17 <- subset(run17, event == "10 Mile")
+mean(run17$net_sec / 60)
+d <- run17[sample(nrow(run17), 100), ]
+myPDF("run17SampHistograms.pdf", 5, 2.8,
+      mar = c(3.5, 3.5, 0.5, 1),
       mgp = c(2.2, 0.55, 0))
-histPlot(d$time,
+histPlot(d$net_sec / 60,
          main = "",
-         xlab = "Time (minutes)",
+         xlab = "Time (Minutes)",
          ylab = "Frequency",
-         yaxs="i",
-         col = COL[1])
-axis(1, at = seq(50, 150, 10))
+         col = COL[1],yaxs="i")
 dev.off()
