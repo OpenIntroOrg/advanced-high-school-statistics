6a0cb87868ad8d942d41740106e7a99af8e6ef6f
Leah Dorazio <leah.dorazio@201506-2126B.local>
2019-08-21 14:37:11 -0700

    updating tables with instructions on how to read

--- extraTeX/tables/figures/chiSquareTail/chiSquareTail.R
+++ extraTeX/tables/figures/chiSquareTail/chiSquareTail.R
@@ -7,7 +7,7 @@ myPDF('chiSquareTail.pdf', 3.5, 2.1,
 X <- seq(0, 25, 0.05)
 Y <- dchisq(X, 3.5)
 
-plot(X, Y, type = 'l', axes = FALSE, xlim = c(0, 15), yaxs="i")
+plot(X, Y, type = 'l', axes = FALSE, xlim = c(0, 15))
 axis(1)
 these <- which(X > 5.79)
 polygon(c(X[these[1]], X[these], X[rev(these)[1]]),
